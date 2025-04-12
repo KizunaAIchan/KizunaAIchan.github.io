e148e9d43d1d68b8be56baa01e2cae788020c837
KizunaAIchan <1309792100@qq.com>
2025-04-12 15:27:12 +0800

    Site updated: 2025-04-12 15:27:12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1744388229457";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1744442832312";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
